1817b067a2c0dc612465a8fded1fee1db693c916
Your Name <you@example.com>
2020-07-11 12:29:05 +0800

    glldddd

---
+++ football/common/client_recv.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: client_recv.h
+	> Author: 
+	> Mail: 
+	> Created Time: Sat 11 Jul 2020 09:47:32 AM CST
+ ************************************************************************/
+
+#ifndef _CLIENT_RECV_H
+#define _CLIENT_RECV_H
+void *do_recv(void *arg);
+#endif
